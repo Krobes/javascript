b849f9478e70d7270d639a0c50ebdf029ff9266f
rbonilla <rafa_lara@hotmail.es>
2023-10-05 09:22:13 +0200

    subida

---
+++ 03-Vanillajs-dom/02-Creator and inserts/main.js
@@ -0,0 +1,16 @@
+const img = document.createElement("img");
+img.src = "img/img_la.jpg";
+img.alt = "LA";
+document.body.appendChild(img);
+
+const h1 = document.createElement("h1");
+h1.textContent = "This is a Heading";
+document.body.appendChild(h1);
+
+const p = document.createElement("p");
+p.textContent = "This is a paragraph";
+document.body.appendChild(p);
+
+const p2 = document.createElement("p");
+p2.textContent = "This is a another paragraph";
+document.body.appendChild(p2);
